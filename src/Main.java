04430e3f8870c7076eccdf3abb55b841b95d7ad9
khshohrukh <shoxruh9902@gmail.com>
2023-10-10 21:58:21 +0500

    Add MathFunctions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
+import statices.MathFunctions;
 import task2.oop.MyTime;
 
 public class Main {
     public static void main(String[] args) {
-        MyTime time = new MyTime();
-        time.setTime(23, 00, 00);
-//        System.out.println(time.toString());
-//        time.nextSecond();
-//        System.out.println(time.toString());
-//        time.nextMinut();
-//        System.out.println(time.toString());
-
-        System.out.println(time.toString());
-        time.previousSecond();
-        System.out.println(time.toString());
-        time.previousMinut();
-        System.out.println(time.toString());
+        MathFunctions mathFunctions = new MathFunctions();
+        mathFunctions.circleA(2.1);
     }
 }
